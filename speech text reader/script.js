253eb282cc08976faaf3d1eb2e996855a3974446
jean.paniagua <jean.paniagua@gorillalogic.com>
2021-07-28 11:27:53 -0600

    js code

--- speech text reader/script.js
+++ speech text reader/script.js
@@ -1,6 +1,6 @@
 const main = document.querySelector('main');
 const voicesSelect = document.getElementById('voices');
-const text = document.getElementById('text');
+const textarea = document.getElementById('text');
 const readBtn = document.getElementById('read');
 const toggleBtn = document.getElementById('toggle');
 const closeBtn = document.getElementById('close');
@@ -69,5 +69,61 @@ function createBox(item) {
         <p class="info">${text}</p>
     `;
 
+    box.addEventListener('click', () => {
+        setTextMessage(text);
+        speakText();
+
+        box.classList.add('active');
+        setTimeout(() => box.classList.remove('active'), 800);
+    });
+
     main.appendChild(box);
 }
+
+const message = new SpeechSynthesisUtterance();
+
+let voices = [];
+
+function getVoices() {
+    voices = speechSynthesis.getVoices();
+  
+    voices.forEach(voice => {
+      const option = document.createElement('option');
+  
+      option.value = voice.name;
+      option.innerText = `${voice.name} ${voice.lang}`;
+  
+      voicesSelect.appendChild(option);
+    });
+  }
+
+function setTextMessage(text) {
+    message.text = text;
+}
+
+function speakText() {
+    speechSynthesis.speak(message);
+}
+
+function setVoice(e) {
+    message.voice = voices.find(voice => voice.name === e.target.value);
+  }
+
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+
+toggleBtn.addEventListener('click', () => document.getElementById('text-box')
+    .classList.toggle('show')
+);
+
+closeBtn.addEventListener('click', () => document.getElementById('text-box')
+    .classList.remove('show')
+);
+
+voicesSelect.addEventListener('change', setVoice);
+
+readBtn.addEventListener('click', () => {
+    setTextMessage(textarea.value);
+    speakText();
+});
+
+getVoices();
