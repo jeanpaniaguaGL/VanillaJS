96143aa58399a1195501cd11ce9192af5c5ddc0b
jean.paniagua <jean.paniagua@gorillalogic.com>
2021-07-29 19:32:09 -0600

    draw objects

--- Breakout game/script.js
+++ Breakout game/script.js
@@ -4,6 +4,94 @@ const rules = document.getElementById('rules');
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
+let score = 0;
+
+const brickRowCount = 9;
+const brickColumnCount = 5;
+
+// Ball properties
+const ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    size: 10,
+    speed: 4,
+    dx: 4,
+    dy: -4
+}
+
+// Draw ball
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y,ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Paddle props
+const paddle = {
+    x: canvas.width / 2 - 40,
+    y: canvas.height - 20,
+    w: 80,
+    h: 10,
+    speed: 8,
+    dx: 0,
+}
+
+// Draw paddle
+function drawPaddle() {
+    ctx.beginPath();
+    ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h)
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+
+function drawScore() {
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+// Brick props
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offsetY: 60,
+    visible: true
+}
+
+// Draw bricks
+function drawBricks() {
+    bricks.forEach(column => {
+        column.forEach(brick => {
+            ctx.beginPath();
+            ctx.rect(brick.x, brick.y, brick.w, brick.h);
+            ctx.fillStyle = brick.visible ? '#095dd' : 'transparent';
+            ctx.fill();
+            ctx.closePath();
+        });
+    });
+}
+
+const bricks = [];
+for(let i = 0; i < brickRowCount; i++) {
+    bricks[i] = []
+    for(let j = 0; j < brickColumnCount; j++) {
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo }
+    }
+}
+
+function draw() {
+    drawBall();
+    drawPaddle();
+    drawScore();
+    drawBricks();
+}
+
 // Event listeners
 rulesBtn.addEventListener('click', () => {
     rules.classList.add('show')
@@ -11,4 +99,6 @@ rulesBtn.addEventListener('click', () => {
 
 closeBtn.addEventListener('click', () => {
     rules.classList.remove('show')
-})
+});
+
+draw();
